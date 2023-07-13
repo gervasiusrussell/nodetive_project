d4e77baec49b1ff7e319b305fe813b440283c390
WinssTjen <winsenind@gmail.com>
2023-07-13 16:53:15 +0700

    test winsen 12

--- nodetive_collab/ContentView.swift
+++ nodetive_collab/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("winsen !")
+            Text("winsenjadf !")
         }
         .padding()
     }
