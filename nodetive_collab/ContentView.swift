2c450c145b2ce2f434459a7b6512d111adf4fb22
Samuelmm777 <masmansamuel@gmail.com>
2023-07-13 16:56:09 +0700

    grgfh

--- nodetive_collab/ContentView.swift
+++ nodetive_collab/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("winsenjadf !")
+            Text("samuel !")
         }
         .padding()
     }
