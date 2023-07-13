6adc453cac9662ad171003e15e57468155374d0f
rara <shyra.alexandria05@gmail.com>
2023-07-13 15:14:27 +0700

    add caendar

---
+++ nodetive_collab/calendarview.swift
@@ -0,0 +1,67 @@
+//
+//  calendarview.swift
+//  nodetive_collab
+//
+//  Created by Shyra Alexandria on 7/13/23.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    @State var selectedDate: Date = Date()
+    
+    var body: some View {
+        
+        VStack {
+            Text("Calendar")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius:10)
+                    .stroke(.gray)
+                    .shadow(color: .black,radius: 5)
+                    .frame(width:350,height:50)
+                    .opacity(0.3)
+                
+                
+                
+                Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
+                    .font(.system(size: 16))
+                    .foregroundColor(.black)
+                    .animation(.spring(), value: selectedDate)
+                    .frame(width: 500)
+                
+                
+            }
+            Divider().frame(height: 1)
+            DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
+                .padding(.horizontal)
+                .datePickerStyle(.graphical)
+                .foregroundColor(Color.accentColor)
+                
+            Divider()
+            
+            Button("Edit Schedule") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        
+        .padding(.vertical, 100)
+        .padding(.horizontal,5)
+        
+        
+    }
+    
+    
+}
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
+
+
+
