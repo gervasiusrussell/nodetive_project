1a464b4bb1cee8d11ea01300c755cc6e1b127b49
russell <gervasius.russell@gmail.com>
2023-07-13 14:32:33 +0700

    Initial Commit

---
+++ nodetive_collab/nodetive_collabApp.swift
@@ -0,0 +1,17 @@
+//
+//  nodetive_collabApp.swift
+//  nodetive_collab
+//
+//  Created by Gervasius Russell on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct nodetive_collabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
