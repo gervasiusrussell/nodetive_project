0f6aef046f4f1098b2398eaf40d6a4fdef2ad045
WinssTjen <winsenind@gmail.com>
2023-07-14 16:48:32 +0700

    tomboy calendar bs dipencet

--- Views/CalendarView.swift
+++ Views/CalendarView.swift
@@ -12,62 +12,67 @@ struct CalendarView: View {
     
     var body: some View {
         
-        VStack {
-            Text("Calendar")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding()
-            
-            Spacer()
-                .frame(width: 0, height: 40)
-            
-            ZStack {
-                RoundedRectangle(cornerRadius:10)
-                    .stroke(.gray)
-                    .shadow(color: .black,radius: 5)
-                    .frame(width:350,height:50)
-                    .opacity(0.3)
-                
-                
-                
-                Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
-                    .font(.system(size: 16))
-                    .foregroundColor(.black)
-                    .animation(.spring(), value: selectedDate)
-                    .frame(width: 500)
-                
-                
-            }
-            Divider().frame(height: 1)
-            DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
-                .padding(.horizontal)
-                .datePickerStyle(.graphical)
-                .foregroundColor(Color.accentColor)
-            
-            Divider()
-            Spacer()
-                .frame(width: 0, height: 30)
-            
-            Button(action: {
-            }) {
-                Text("Edit Schedule")
-                    .font(.body)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
+        NavigationView{
+            VStack {
+                Text("Calendar")
+                    .font(.title)
+                    .fontWeight(.bold)
                     .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(.accentColor)
-                    )
+                
+                Spacer()
+                    .frame(width: 0, height: 40)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius:10)
+                        .stroke(.gray)
+                        .shadow(color: .black,radius: 5)
+                        .frame(width:350,height:50)
+                        .opacity(0.3)
+                    
+                    
+                    
+                    Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
+                        .animation(.spring(), value: selectedDate)
+                        .frame(width: 500)
+                    
+                    
+                }
+                Divider().frame(height: 1)
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
+                    .padding(.horizontal)
+                    .datePickerStyle(.graphical)
+                    .foregroundColor(Color.accentColor)
+                
+                Divider()
+                Spacer()
+                    .frame(width: 0, height: 30)
+                
+                Button(action: {
+                }) {
+                    NavigationLink(destination: AddScheduleView()){
+                        Text("Edit Schedule")
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundColor(.accentColor))
+                    }
+                }
+  
+                Spacer()
+                    .frame(width: 0, height: 120)
             }
-            Spacer()
-                .frame(width: 0, height: 120)
         }
-        
     }
 
 }
+                                   
+                                   
 struct CalendarView_Previews: PreviewProvider {
     static var previews: some View {
         CalendarView()
