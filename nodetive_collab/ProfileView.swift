044896fad1ecac26cea19df90f7890e35c3b0f7c
Jennifer <jennifermarcellyn52@gmail.com>
2023-07-14 17:40:28 +0700

    Profileview

--- nodetive_collab/ProfileView.swift
+++ nodetive_collab/ProfileView.swift
@@ -1,15 +1,155 @@
 //
 //  ProfileView.swift
-//  nodetive_collab
+//  Nodetive
 //
-//  Created by Winsen Tjen on 13/07/23.
+//  Created by Jennifer Marcellyn Cen on 14/07/23.
 //
 
 import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .frame(width: 70, height: 70)
+                    VStack (alignment: .leading){
+                        Text("Gervasius Russell")
+                            .font(.system(size: 20, weight: .bold))
+                        Text("Supreme Hustler")
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                }
+                Rectangle()
+                    .frame(width: 500, height: 5)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 20)
+                VStack {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Spacer()
+                            .frame(width: 30, height: 0)
+                        Text("Personal Data")
+                            .font(.system(size: 18, weight: .medium))
+                        Spacer()
+                            .frame(width: 118, height: 0)
+                        NavigationLink{
+                            IntroView()
+                        } label:{
+                            Image(systemName: "arrow.forward")
+                                .resizable()
+                                .frame(width: 30, height: 25)
+                                .foregroundColor(.black)
+                        }
+                    }.padding(.bottom, 20)
+                    Rectangle()
+                        .frame(width: 343, height: 1)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 20)
+                    HStack {
+                        Image(systemName: "gear")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Spacer()
+                            .frame(width: 30, height: 0)
+                        Text("Settings")
+                            .font(.system(size: 18, weight: .medium))
+                        Spacer()
+                            .frame(width: 164, height: 0)
+                        NavigationLink{
+                            IntroView()
+                        } label:{
+                            Image(systemName: "arrow.forward")
+                                .resizable()
+                                .frame(width: 30, height: 25)
+                                .foregroundColor(.black)
+                        }
+                    }.padding(.bottom, 20)
+                    Rectangle()
+                        .frame(width: 343, height: 1)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 20)
+                    Rectangle()
+                        .frame(width: 500, height: 5)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 20)
+                }
+                HStack {
+                    Image(systemName: "ticket")
+                        .resizable()
+                        .frame(width: 50, height: 35)
+                    Spacer()
+                        .frame(width: 30, height: 0)
+                    Text("Referral Code")
+                        .font(.system(size: 18, weight: .medium))
+                    Spacer()
+                        .frame(width: 118, height: 0)
+                    NavigationLink{
+                        IntroView()
+                    } label:{
+                        Image(systemName: "arrow.forward")
+                            .resizable()
+                            .frame(width: 30, height: 25)
+                            .foregroundColor(.black)
+                    }
+                }.padding(.bottom, 20)
+                Rectangle()
+                    .frame(width: 343, height: 1)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 20)
+                HStack {
+                    Image(systemName: "crown")
+                        .resizable()
+                        .frame(width: 50, height: 40)
+                    Spacer()
+                        .frame(width: 30, height: 0)
+                    Text("Subscription")
+                        .font(.system(size: 18, weight: .medium))
+                    Spacer()
+                        .frame(width: 128, height: 0)
+                    NavigationLink{
+                        IntroView()
+                    } label:{
+                        Image(systemName: "arrow.forward")
+                            .resizable()
+                            .frame(width: 30, height: 25)
+                            .foregroundColor(.black)
+                    }
+                }.padding(.bottom, 20)
+                Rectangle()
+                    .frame(width: 343, height: 1)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 20)
+                HStack {
+                    Image(systemName: "questionmark.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Spacer()
+                        .frame(width: 30, height: 0)
+                    Text("Help & Support")
+                        .font(.system(size: 18, weight: .medium))
+                    Spacer()
+                        .frame(width: 106, height: 0)
+                    NavigationLink{
+                        IntroView()
+                    } label:{
+                        Image(systemName: "arrow.forward")
+                            .resizable()
+                            .frame(width: 30, height: 25)
+                            .foregroundColor(.black)
+                    }
+                }.padding(.bottom, 20)
+                Rectangle()
+                    .frame(width: 343, height: 1)
+            }
+            .padding(.all,25)
+        }
+        
     }
 }
 
@@ -18,3 +158,4 @@ struct ProfileView_Previews: PreviewProvider {
         ProfileView()
     }
 }
+
