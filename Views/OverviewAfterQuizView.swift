ac9d822d886cd94d23b5cf07f08b5319fd6bc0b3
Samuelmm777 <masmansamuel@gmail.com>
2023-07-15 09:36:44 +0700

    y

---
+++ Views/OverviewAfterQuizView.swift
@@ -0,0 +1,92 @@
+//
+//  OverviewAfterQuizView.swift
+//  nodetive_collab
+//
+//  Created by Samuel Matthew Masman on 15/07/23.
+//
+
+import SwiftUI
+
+struct OverviewAfterQuizView: View {
+    var body: some View {
+        VStack{
+            Text("Your undefined is below 40%, Good Job!")
+                .fontWeight(.bold)
+            
+            Spacer()
+                .frame(height:70)
+            
+            HStack{
+                Spacer()
+                    .frame(width: 0)
+                Text("Productivity ")
+                    .font(.body)
+                Spacer()
+                    .frame(width: 35)
+                Text("Sleep")
+                    .font(.body)
+                Spacer()
+                    .frame(width: 55)
+                Text("Leisure")
+                    .font(.body)
+                Spacer()
+                    .frame(width: 10)
+            }
+            
+            HStack{
+                Rectangle()
+                    .frame(width: 20, height: 280)
+                    .foregroundColor(Color("silver"))
+                    .cornerRadius(30)
+                    .overlay(
+                        VStack {
+                            Spacer()
+                            Rectangle()
+                                .frame(width: 20, height: 250)
+                                .foregroundColor(Color("prod"))
+                                .cornerRadius(30)
+                        }
+                    )
+                
+                Spacer()
+                    .frame(width: 90)
+                Rectangle()
+                    .frame(width: 20, height: 280)
+                    .foregroundColor(Color("silver"))
+                    .cornerRadius(30)
+                    .overlay(
+                        VStack{
+                            Spacer()
+                            Rectangle()
+                                .frame(width: 20, height: 180)
+                                .foregroundColor(Color("sleep"))
+                                .cornerRadius(30)
+                        }
+                    )
+                Spacer()
+                    .frame(width: 90)
+                Rectangle()
+                    .frame(width: 20, height: 280)
+                    .foregroundColor(Color("silver"))
+                    .cornerRadius(30)
+                    .overlay(
+                        VStack{
+                            Spacer()
+                            Rectangle()
+                                .frame(width: 20, height: 220)
+                                .foregroundColor(Color("leisure"))
+                                .cornerRadius(30)
+                        }
+                    )
+            }
+        }
+        
+        Spacer()
+        
+    }
+}
+struct OverviewAfterQuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        OverviewAfterQuizView()
+    }
+}
